fba36ec122ca588a9c291ea0c0835cfb1f87dfaa
Areum Jo <areumjo1@gmail.com>
2020-05-26 16:22:26 -0700

    Updates

---
+++ precache-manifest.fb62b0cdc3ad8ed42f10ccbf09d978f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f839d1de5c6649e7ad237493aecdf079",
+    "url": "/fcc-10-js-calculator/index.html"
+  },
+  {
+    "revision": "775cfee7988ab05d1d75",
+    "url": "/fcc-10-js-calculator/static/css/main.3bb0226a.chunk.css"
+  },
+  {
+    "revision": "dca3b58ea4883ff702ef",
+    "url": "/fcc-10-js-calculator/static/js/2.90ed3c21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fcc-10-js-calculator/static/js/2.90ed3c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775cfee7988ab05d1d75",
+    "url": "/fcc-10-js-calculator/static/js/main.0a1e453e.chunk.js"
+  },
+  {
+    "revision": "076301f1b9b7fcf4d7ce",
+    "url": "/fcc-10-js-calculator/static/js/runtime-main.fe3ea061.js"
+  }
+]);
